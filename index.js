e605e0d9e34ea1b4e904a7188589731e86d533b1
M. Nasimul Haque <nasim.haque@gmail.com>
2016-09-30 10:52:46 +0100

    expose provideInterceptorService

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ exports.InterceptedRequest = require('./lib/intercepted-request').InterceptedReq
 exports.InterceptedResponse = require('./lib/intercepted-request').InterceptedResponse;
 exports.InterceptorOptions = require('./lib/interceptor-options').InterceptorOptions;
 exports.InterceptorService = require('./lib/interceptor-service').InterceptorService;
+exports.provideInterceptorService = require('./lib/interceptor-provide').provideInterceptorService;
 exports.Interceptor = require('./lib/interceptor').Interceptor;
