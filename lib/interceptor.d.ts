068060bbf02a5bfd8590438ba66d1ba03e613c98
voliva <olivarra1@gmail.com>
2016-10-10 23:38:38 +0200

    Updated to Typescript 2.0

--- lib/interceptor.d.ts
+++ lib/interceptor.d.ts
@@ -1,5 +1,5 @@
-import { InterceptedRequest } from "./intercepted-request.ts";
-import { InterceptedResponse } from "./intercepted-response.ts";
+import { InterceptedRequest } from "./intercepted-request";
+import { InterceptedResponse } from "./intercepted-response";
 import { Observable } from 'rxjs/Rx';
 export interface Interceptor {
     interceptBefore?(request: InterceptedRequest): Observable<InterceptedRequest> | InterceptedRequest;
