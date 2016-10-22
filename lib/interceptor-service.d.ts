925c1cccb3e7b21eba018f010be6c9abea6e896f
voliva <olivarra1@gmail.com>
2016-10-22 10:26:29 +0200

    Use new angular Http API
    Fixes #6 #7

--- lib/interceptor-service.d.ts
+++ lib/interceptor-service.d.ts
@@ -1,4 +1,4 @@
-import { ConnectionBackend, Http, Response, RequestOptions } from '@angular/http';
+import { ConnectionBackend, Http, Request, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { InterceptorOptions } from "./interceptor-options";
 import { Interceptor } from "./interceptor";
@@ -11,11 +11,36 @@ export declare class InterceptorService extends Http {
     */
     addInterceptor(interceptor: Interceptor): void;
     /** Parent overrides **/
-    request(url: string, options?: InterceptorOptions): Observable<Response>;
+    private httpRequest(request);
+    request(url: string | Request, options?: InterceptorOptions): Observable<Response>;
+    /**
+     * Performs a request with `get` http method.
+     */
     get(url: string, options?: InterceptorOptions): Observable<Response>;
-    post(url: string, body: string, options?: InterceptorOptions): Observable<Response>;
-    put(url: string, body: string, options?: InterceptorOptions): Observable<Response>;
+    /**
+     * Performs a request with `post` http method.
+     */
+    post(url: string, body: any, options?: InterceptorOptions): Observable<Response>;
+    /**
+     * Performs a request with `put` http method.
+     */
+    put(url: string, body: any, options?: InterceptorOptions): Observable<Response>;
+    /**
+     * Performs a request with `delete` http method.
+     */
     delete(url: string, options?: InterceptorOptions): Observable<Response>;
+    /**
+     * Performs a request with `patch` http method.
+     */
+    patch(url: string, body: any, options?: InterceptorOptions): Observable<Response>;
+    /**
+     * Performs a request with `head` http method.
+     */
+    head(url: string, options?: InterceptorOptions): Observable<Response>;
+    /**
+     * Performs a request with `options` http method.
+     */
+    options(url: string, options?: InterceptorOptions): Observable<Response>;
     /** Private functions **/
     private runBeforeInterceptors(params);
     private runAfterInterceptors(response, startOn);
